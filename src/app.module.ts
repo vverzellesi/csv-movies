699a4316a9051fda11d52bc7a0f7f02c4e6fc536
Victor Bitelman Verzellesi <v.verzellesi@gmail.com>
2023-11-07 17:40:01 -0300

    Convert CSV file to JSON once the application starts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppService, CsvToJsonService } from './app.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CsvToJsonService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import * as csv from 'csvtojson';
+
+
+@Injectable()
+export class CsvToJsonService implements OnModuleInit {
+  async onModuleInit() {
+    try {
+      Logger.log('Converting CSV file to JSON...');
+      const jsonMovies = await csv({ delimiter: ';' }).fromFile('src/data/movielist.csv');
+      //TODO: save to db
+    } catch (error) {
+      throw new Error(`Error converting CSV to JSON: ${error.message}`);
+    }
+  }
+}
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getMovies(): Promise<any[]> {
+    return [];
   }
 }
