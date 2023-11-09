01d0773b0e643d2741f25d5ae3df78855a582552
Victor Bitelman Verzellesi <v.verzellesi@gmail.com>
2023-11-09 02:29:36 -0300

    Import CsvToJsonService in movie module

--- src/movie/movie.module.ts
+++ src/movie/movie.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CsvToJsonService } from '../app.service';
 import { LokijsService } from '../db/lokijs.service';
 import { MovieController } from './movie.controller';
 import { MovieService } from './movie.service';
 
 @Module({
     controllers: [MovieController],
-    providers: [MovieService, LokijsService],
+    providers: [MovieService, LokijsService, CsvToJsonService],
 })
 export class MovieModule { }
